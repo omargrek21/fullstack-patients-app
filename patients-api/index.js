4f4b936515cd09230bf7525b2451ee3fb11adef4
Omar Perez <omarmk6@gmail.com>
2018-06-21 21:28:14 +0000

    parsing .csv data using csv-fast library

--- patients-api/index.js
+++ patients-api/index.js
@@ -1,9 +1,25 @@
 var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
-    patientRoutes = require('./routes/patients');
+    patientRoutes = require('./routes/patients'),
+    fs = require('fs'),
+    csv = require('fast-csv');
 
 app.get('/', function(req, res){
+    console.log("starting read .csv file");
+    let patientsArr = [];
+    
+    var stream = fs.createReadStream("csvexample.csv");
+    csv
+     .fromStream(stream, {headers: ["dni", "titular_dni", "full_name", "birth_date", "location", "type", "owner", "branch", "insurance_company"]})
+     .on("data", function(data){
+         patientsArr.push(data);
+     })
+     .on("end", function(){
+         console.log("done");
+         console.log(patientsArr);
+     });
+    
     res.send("hello from the root route");
 });
 
