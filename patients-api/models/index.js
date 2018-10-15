699f1dac8858ed9647c1cc07a544a4e0a78af1d2
Omar Perez <omarmk6@gmail.com>
2018-10-15 18:30:26 +0000

    testing mongodb auth

--- patients-api/models/index.js
+++ patients-api/models/index.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose');
 mongoose.set('debug', false);
-mongoose.connect('mongodb://localhost/validator', {keepAlive: true});
-mongoose.Promise = Promise; //allow Promise sintax
+mongoose.connect(`mongodb://${process.env.DB_CREDENTIALS}@localhost/validator`, {keepAlive: true});
+mongoose.Promise = Promise;
 module.exports.Patient = require('./patient');
 module.exports.User = require('./user');
 module.exports.Client = require('./client');
